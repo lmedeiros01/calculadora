3a7049b7efb9c9a297d9344264063f5a01820d3d
Leonardo Medeiros <leo_medeiros@live.com>
2023-05-26 18:56:10 -0300

    cores alteradas da calculadora

--- App.js
+++ App.js
@@ -72,12 +72,12 @@ export default function App() {
 
                 {buttons.map((button) =>
                     button === '=' ?
-                        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#3dd0e3' }]}>
+                        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, { backgroundColor: '#00008B' }]}>
                             <Text style={[styles.textButton, { color: "white", fontSize: 30 }]}>{button}</Text>
                         </TouchableOpacity>
                         :
                         <TouchableOpacity onPress={() => handleInput(button)} key={button} style={styles.button}>
-                            <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'black' : '#0093a6' }]}>{button}</Text>
+                            <Text style={[styles.textButton, { color: typeof (button) === 'number' ? 'white' : '#708090' }]}>{button}</Text>
                         </TouchableOpacity>
                 )}
             </View>
@@ -92,17 +92,17 @@ const styles = StyleSheet.create({
     results: {
         flex: 2,
         justifyContent: "center",
-        backgroundColor: "#f5f5f5"
+        backgroundColor: "#00008B"
     },
     resultText: {
-        color: "#282F38",
+        color: "#FFFFFF",
         fontSize: 32,
         fontWeight: "bold",
         padding: 12,
         textAlign: "right"
     },
     historyText: {
-        color: "#7c7c7c",
+        color: "#708090",
         fontSize: 20,
         marginRight: 10,
         alignSelf: 'flex-end',
@@ -112,7 +112,7 @@ const styles = StyleSheet.create({
         flexWrap: 'wrap',
     },
     button: {
-        backgroundColor: 'white',
+        backgroundColor: '#191970',
         alignItems: 'center',
         justifyContent: 'center',
         minWidth: 90,
@@ -120,7 +120,7 @@ const styles = StyleSheet.create({
         flex: 2,
     },
     textButton: {
-        color: "#7c7c7c",
+        color: "#FFFFFF",
         fontSize: 20,
     }
 });
